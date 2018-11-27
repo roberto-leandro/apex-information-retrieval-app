6e9fb6b6541b2411b9b6ddad33926e74a70e2db0
Nathan9604 <40476781+Nathan9604@users.noreply.github.com>
2018-11-27 12:17:07 -0600

    Update startup.sql

--- startup.sql
+++ startup.sql
@@ -15,8 +15,8 @@ CREATE VIEW v_biblioteca AS
     nombre_archivo AS Nombre_del_archivo, 
     charset_archivo AS Set_de_caracteres, 
     fecha_carga AS Fecha_de_carga, 
-    ult_act AS Última_actualización,
-    sys.dbms_lob.getlength(documento) AS Tamaño,
+    ult_act AS Ãšltima_actualizaciÃ³n,
+    sys.dbms_lob.getlength(documento) AS TamaÃ±o,
     1 AS Descargar
     FROM Biblioteca;
 
@@ -34,3 +34,23 @@ create table TEMA (
        PESO_CALIDAD number,
        parent number);
 
+CREATE INDEX "TEXTDEV"."DOCUMENTO_IDX" ON "TEXTDEV"."BIBLIOTECA" ("DOCUMENTO") 
+   INDEXTYPE IS "CTXSYS"."CONTEXT"  PARAMETERS ('lexer MYLEXER stoplist CTXSYS.EMPTY_STOPLIST DATASTORE CTXSYS.DEFAULT_DATASTORE');
+   
+CREATE TABLE Biblioteca(
+    titulo              varchar(500)     PRIMARY KEY,
+    fecha_creacion      date,
+    notas               varchar(1000),
+    tipo_documento      varchar(18)     CHECK( tipo_documento IN ('Articulo', 'Brochure', 'Obra Literaria', 'Ensayo', 
+                                        'Presentacion', 'Manual', 'Documento Tecnico', 'Otro') ) NOT NULL,
+                                        
+    tipo_archivo        varchar(10)     CHECK( tipo_archivo IN ('PDF', 'Word', 'Excel', 'PowerPoint', 'Texto', 'HTML', 
+                                        'Keynote', 'Pages', 'Numbers', 'Otro') ),
+                                        
+    disponible          varchar(2)     CHECK( disponible IN ('si', 'no')),
+    autores             varchar(300),
+    fecha_carga         date,
+    tam                 varchar(10),
+    usuario             varchar(30),
+    documento           blob    NOT NULL
+);
